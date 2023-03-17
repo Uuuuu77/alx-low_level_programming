b6fb4f6d226215bb2f6ae85f1c6addf76a510835
John <johnmnjuguna89@gmail.com>
2023-03-17 13:28:44 -0700

    100-print_comb3.c file

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,13 +6,13 @@
 */
 int main(void)
 {
-char alp(52) = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 int i;
 
 for (i = 0; i < 52; i++)
 {
-	putchar(alp(i));
+	putchar("alp", (i));
 }
-putchar("\n");
+putchar('\n');
 return (0);
 }
