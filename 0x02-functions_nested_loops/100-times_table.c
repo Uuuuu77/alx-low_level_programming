9e01767b1bd5bd140e6652c1c4af66145c843ff4
John <johnmnjuguna89@gmail.com>
2023-03-22 07:14:26 -0700

    100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints the n time table, starting from 0
+ * @n: number of the time table
+*/
+void print_times_table(int n)
+{
+	int i, y, k;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (y = 0; y <= n; y++)
+			{
+				k = y * i;
+				if (y == 0)
+				{
+					_putchar(k + '0');
+				}
+				else if (k < 10 && y != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
+				}
+				else if (k >= 10 && k < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				}
+				else if (k >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10 + '0'));
+				       _putchar((k % 10) + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
