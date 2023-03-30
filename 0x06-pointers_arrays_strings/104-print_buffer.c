da2eca465b67f938692b7a5e662ccaa2d28f8613
John <johnmnjuguna89@gmail.com>
2023-03-30 06:18:43 -0700

    prints a buffer

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * print_buffer - prints buffer.
+ * @b: buffer
+ * @size: the size to be printed
+*/
+void print_buffer(char *b, int size)
+{
+	int o, g, p;
+
+	o = 0;
+
+	if (size <= 0)
+	{
+		printf("\n");
+		return;
+	}
+	while (o < size)
+	{
+		g = size - o < 10 ? size - o : 10;
+		printf("%08x", o);
+		for (p = 0; p < 10; p++)
+		{
+			if (p < g)
+				printf("%02x", *(b + o + p));
+			else
+				printf(" ");
+			if (p % 2)
+			{
+				printf(" ");
+			}
+		}
+		for (p = 0; p < g; p++)
+		{
+			int c = *(b + o + p);
+
+			if (c < 32 || c > 132)
+			{
+				c = '.';
+			}
+			printf("%c", c);
+		}
+		printf("\n");
+		o += 10;
+	}
+}
