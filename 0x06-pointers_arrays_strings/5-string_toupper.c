13568c75683c9f38a9cdaa9212bf67df3baced7b
John <johnmnjuguna89@gmail.com>
2023-03-29 23:51:31 -0700

    5-string_toupper.c file that changes lowercase to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *string_toupper - changes lowercase to uppercase
+ * @m: input pointer
+ * Return: m (success)
+*/
+char *string_toupper(char *m)
+{
+	int b = 0;
+
+	while (m[b] != '\0')
+	{
+		if (m[b] >= 'a' && m[b] <= 'z')
+		{
+			m[b] = m[b] - 32;
+		}
+		m++;
+	}
+	m[b] = '\0';
+	return (m);
+}
