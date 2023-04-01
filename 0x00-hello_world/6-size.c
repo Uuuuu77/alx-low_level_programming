986d9256ae9c30d73f84264a209f22d8e666cacd
John <johnmnjuguna89@gmail.com>
2023-04-01 03:15:58 -0700

    fixed bug

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,6 +10,7 @@ int main(void)
 	long int u;
 	long long int h;
 	float t;
+
 	printf("Size of a char: %ld byte(s)\n", sizeof(g));
 	printf("Size of an int: %ld byte(s)\n", sizeof(b));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(u));
