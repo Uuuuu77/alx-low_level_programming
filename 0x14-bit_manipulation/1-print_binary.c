4d61223b8fa7c5998fef0dee607bf5966685e8c5
John Njuguna <johnmnjuguna89@gmail.com>
2023-07-06 09:13:06 -0700

    Prints number in Binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_binary - Entry point -> Basically prints binary rep of a number.
+ * @n: input number to be printed in binary
+*/
+void print_binary(unsigned long int n)
+{
+	int b, num = 0;
+	unsigned long int current;
+
+	for (b = 63; b >= 0; b--)
+	{
+		current = n >> b;
+		if (current & 1)
+		{
+			_putchar('1');
+			num++;
+		}
+		else if (num)
+			_putchar('0');
+	}
+}
